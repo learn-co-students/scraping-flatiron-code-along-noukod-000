7f2341c563998cbe5c8e155b97cab637d1b13c0c
AnnJohn <ann@flatironschool.com>
2016-01-13 22:17:32 -0500

    added print_courses method

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,6 +5,16 @@ require_relative './course.rb'
 
 class Scraper
   
+    def print_courses
+    self.make_courses
+    Course.all.each do |course|
+      if course.title
+        puts "Title: #{course.title}"
+        puts "  Schedule: #{course.schedule}"
+        puts "  Description: #{course.description}"
+      end
+    end
+  
 end
 
 
