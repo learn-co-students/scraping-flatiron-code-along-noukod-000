d566f3d2427b460a3ac837a530ab2582ab2e3029
vicfriedman <victoria.m.friedman@gmail.com>
2016-02-26 13:52:18 -0500

    solution

--- lib/course.rb
+++ lib/course.rb
@@ -1,2 +1,18 @@
+class Course
+  attr_accessor :title, :schedule, :description
 
+  @@all = []
+
+  def initialize
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.reset_all
+    @@all.clear
+  end
+end
 
--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,11 +1,12 @@
 require 'nokogiri'
 require 'open-uri'
-
+require 'pry'
 require_relative './course.rb'
 
 class Scraper
+  attr_accessor :doc, :course_info
   
-    def print_courses
+  def print_courses
     self.make_courses
     Course.all.each do |course|
       if course.title
@@ -15,8 +16,30 @@ class Scraper
       end
     end
   end
+
+  def get_page
+    html = open("http://flatironschool.com/courses")
+    @doc = Nokogiri::HTML(html)
+  end
+
+  def get_courses
+   @course_info =  get_page.css('.post')
+  
+  end
+
+  def make_courses
+   get_courses.each do |course|
+      c = Course.new
+      c.title = course.children.children.first.text
+      c.schedule = course.children.children[1].text
+      c.description = course.children.children[2].text
+      Course.all << c
+    end
+
+  end
+
+
   
 end
-
 
 
