0e7d847ad5ce0a1e792d181f416b4eca825e9754
Jason Decker <curiositypaths@users.noreply.github.com>
2017-11-27 17:39:08 -0500

    Improve scraper test coverage

--- spec/scraper_spec.rb
+++ spec/scraper_spec.rb
@@ -1,15 +1,15 @@
 require_relative './spec_helper'
 require_relative '../lib/scraper.rb'
 
-describe "Scraper" do 
+describe "Scraper" do
 
   let(:scraper) {Scraper.new}
-  
+
   describe "#get_page" do
-    it "uses Nokogiri to get the HTML from a web page" do 
+    it "uses Nokogiri to get the HTML from a web page" do
       doc = scraper.get_page
       expect{scraper.get_page}.to_not raise_error
-      expect(doc).to be_a(Nokogiri::HTML::Document) 
+      expect(doc).to be_a(Nokogiri::HTML::Document)
     end
   end
 
@@ -17,9 +17,11 @@ describe "Scraper" do
     Course.reset_all
   end
 
-  describe "#get_courses" do 
-    it "uses a CSS selector to return an array of Nokogiri XML elements representing the courses described on the web page we are scraping" do 
+  describe "#get_courses" do
+    it "uses a CSS selector to return an array of Nokogiri XML elements representing the courses described on the web page we are scraping" do
       course_offerings = scraper.get_courses
+      expect(course_offerings.class).to be(Nokogiri::XML::NodeSet)
+      expect(course_offerings.empty?).not_to be(true)
       course_offerings.each do |course_offering|
         expect(course_offering).to be_a(Nokogiri::XML::Element)
         expect{course_offering.css("h2").text}.to_not raise_error
@@ -27,14 +29,16 @@ describe "Scraper" do
     end
   end
 
-  describe "#make_courses" do 
-    it "iterates over the courses array returned by #get_courses and creates a new Course instance out of each array element." do 
+  describe "#make_courses" do
+    it "iterates over the courses array returned by #get_courses and creates a new Course instance out of each array element." do
       Course.reset_all
       courses = scraper.make_courses
+      expect(Course.all.class).to be(Array)
+      expect(Course.all.empty?).not_to be(true)
       Course.all.each do |course|
         expect(course.title).to be_a String
         expect(course.schedule).to be_a String
-        expect(course.description).to be_a String 
+        expect(course.description).to be_a String
       end
     end
   end
